ae1756440b05e10f5d2e4e102d9758b381a8cc70
jenniferishimwe <159912041+jenniferishimwe@users.noreply.github.com>
2024-03-21 05:39:10 -0700

    Add files via upload

---
+++ InventoryManagement.java
@@ -0,0 +1,54 @@
+package java_assignment;
+import java.util.Scanner;
+public class InventoryManagement {
+    public static void main(String[] args) {
+        int len;
+        double updatequantity;
+                String new_crop,crop1[]={};
+        Scanner in=new Scanner(System.in);
+        System.out.println("input the length of array");
+        len=in.nextInt();
+        String[] NameOfCrops=new String[len+1];
+        System.out.println("enter the"+" "+len+"crops");
+                for(int i=0;i<NameOfCrops.length-1;i++){
+                      NameOfCrops[i]=in.next();
+                     
+        }
+                System.out.println("input the length of array of crops");
+        int leng=in.nextInt();
+       int[] quantity=new int[leng+1];
+          System.out.println("enter the"+" "+len+"quantities");
+                for(int i=0;i<quantity.length-1;i++){
+                      quantity[i]=in.nextInt();
+                     
+        }       
+//Double[] Quantity={35.0,20.0};
+//for(int i=0;i<NameOfCrops.length;i++){
+//            System.out.println(Quantity[i]);
+//        }
+//      
+          System.out.println("1.Adding new crop");
+         System.out.println("2.update quantity new crop");
+         System.out.println("3.disply current inventory");
+        System.out.println("Choose From This above Menu by entering the number:");
+        int num=in.nextInt();
+        if(num>1){
+         if(num==1){
+           new_crop=in.next();
+    NameOfCrops[len]=new_crop;
+    for(int i=0;i<NameOfCrops.length-1;i++){
+                          System.out.println(NameOfCrops[i]); 
+        }}
+    if(num==2){
+    System.out.println("update quantity");
+        for(int i=0;i<quantity.length-1;i++){
+        quantity[i]=in.nextInt();     
+        }
+    }
+    
+    }
+         }
+        
+    }
+
+
